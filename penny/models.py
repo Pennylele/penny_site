dbbd714334a78cb8d3285e10b4d121baf542396d
penny <fangle0121@gmail.com>
2018-10-21 21:44:47 -0700

    commit again

--- penny/models.py
+++ penny/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# class Blog(modules.Module):
+# 	created_at = modules.DateTimeField(auto_now_add=True)
+#	title = modules.CharField(max_length=255)
+#	body = modules.TextField()
+
+#	def __str__(self):
+#		return self.title
